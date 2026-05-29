501a531dec92059b2dc6cfeb9917deb09f8471fc
pismeno <krystof.kloz@gmail.com>
2026-05-29 12:56:27 +0200

    feat: option to set default error view

--- src/lemur/error_handling.py
+++ src/lemur/error_handling.py
@@ -1,7 +1,12 @@
 from lemur.templating import make_view
 from lemur.wrappers import HTTPException
 
+__defafult_error_view: str
 __error_views = {}
+
+def set_default_error_view(view_path: str) -> None:
+    global __defafult_error_view
+    __defafult_error_view = view_path
 
 def set_error_view(status_code: int, view_path: str) -> None:
     __error_views[status_code] = view_path
@@ -9,7 +14,12 @@ def set_error_view(status_code: int, view_path: str) -> None:
 def make_error_view(e: Exception) -> str:
     status_code = e.status_code if isinstance(e, HTTPException) else 500
     view_path = __error_views.get(status_code)
+
+    if not view_path:
+        view_path = __defafult_error_view
+
     if not view_path:
         view_path = "/lemur/templates/error"
+
     message = str(e) or "An unexpected error occurred."
     return make_view(view_path, {"status_code": status_code, "message": message})
