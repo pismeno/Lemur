da5234579bbc6f69d48999cce4a6aee408385c96
pismeno <krystof.kloz@gmail.com>
2026-06-01 14:53:18 +0200

    fix(project template): updating path variables inside wsgi.py script

--- src/lemur/project_template/wsgi.py
+++ src/lemur/project_template/wsgi.py
@@ -1,8 +1,21 @@
 from pathlib import Path
-from lemur.utils.assets import set_project_root 
+import lemur
+import lemur.utils.assets as lemur_assets
 
-BASE_DIR = Path(__file__).resolve().parent
+PROJECT_ROOT_PATH = Path(__file__).resolve().parent
+PRIVATE_PATH = PROJECT_ROOT_PATH / "private"
+PUBLIC_PATH = PROJECT_ROOT_PATH / "public"
 
-set_project_root(BASE_DIR)
+LEMUR_ROOT = Path(lemur.__file__).resolve().parent
+LEMUR_PRIVATE_PATH = LEMUR_ROOT / "private"
+LEMUR_PUBLIC_PATH = LEMUR_ROOT / "public"
+
+lemur_assets.PROJECT_ROOT_PATH = PROJECT_ROOT_PATH
+lemur_assets.PRIVATE_PATH = PRIVATE_PATH
+lemur_assets.PUBLIC_PATH = PUBLIC_PATH
+
+lemur_assets.LEMUR_ROOT = LEMUR_ROOT
+lemur_assets.LEMUR_PRIVATE_PATH = LEMUR_PRIVATE_PATH
+lemur_assets.LEMUR_PUBLIC_PATH = LEMUR_PUBLIC_PATH
 
 from lemur.kernel import application
